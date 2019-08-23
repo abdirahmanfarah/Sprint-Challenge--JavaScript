0de897d52f3ad0fed64548b96c84181e1992db53
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2019-08-23 10:49:40 -0700

    final

--- challenges/classes.js
+++ challenges/classes.js
@@ -18,14 +18,18 @@ class cubeMaker extends cuboidMaker {
   constructor(attributes){
     super(attributes)
   }
+  surfaceArea1(){
+
+  }
 };
 
-// const cube = new cuboidMaker({
-//   length: 5,
-//   width: 4,
-//   height: 10
-// });
-// console.log(cube.volume());
+
+const cube = new cubeMaker({
+  length: 5,
+  width: 4,
+  height: 10
+});
+console.log(cube.volume());
 
 const cuboid = new cuboidMaker({
   length: 4,
@@ -39,3 +43,4 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
